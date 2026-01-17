8758e2a738ff6bd94ac939e6661348c42a2436e5
Teesha Ramchandani <teeshar@iitbhilai.ac.in>
2026-01-17 12:14:30 +0530

    datatypes

--- day_1_test.js
+++ day_1_test.js
@@ -1,6 +1,8 @@
 console.log("Test file for Day 1 loaded successfully.");
 
 // Variables
+
+
 // when variable is assigned with const keyword , it cannot be reassigned.
 const accountID = "12234322";
 
@@ -16,3 +18,17 @@ accountEmail="hello.example.com"; // Reassignment is allowed for let
 accountPassword="newPassword456"; // Reassignment is allowed for var
 account_city="Los Angeles"; // Implicit global variable assignment
 console.table([accountID, accountEmail, accountPassword, account_city,account_state]);
+
+
+
+// Data Types and ECMA standards
+
+ " use strict"; // Allows to use latest features of JS and avoid some pitfalls of older versions 
+
+
+// alert(3+3); we are using nodejs to run this file so alert will not work here. 
+
+//primitive data types : number ,string,boolean,null,undefined,symbol(bigint)
+
+console.log(typeof null); // object (this is a known quirk in JavaScript)
+console.log(typeof undefined);

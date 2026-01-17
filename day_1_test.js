9c2fe856020a7383df19b0bed6436a56d421da5e
Teesha Ramchandani <teeshar@iitbhilai.ac.in>
2026-01-17 11:17:22 +0530

    variables_learned

--- day_1_test.js
+++ day_1_test.js
@@ -1,1 +1,18 @@
 console.log("Test file for Day 1 loaded successfully.");
+
+// Variables
+// when variable is assigned with const keyword , it cannot be reassigned.
+const accountID = "12234322";
+
+let accountEmail = "teesha@example.com";
+
+let account_state; // Implicit global variable declaration
+// var has an issue of scope, it does not understand the concept of scope like let and hence it is better to avoid using var.
+var accountPassword = "securePassword123";
+account_city="New York";
+
+//accountID="54321"; // This will cause an error because accountID is declared with const
+accountEmail="hello.example.com"; // Reassignment is allowed for let
+accountPassword="newPassword456"; // Reassignment is allowed for var
+account_city="Los Angeles"; // Implicit global variable assignment
+console.table([accountID, accountEmail, accountPassword, account_city,account_state]);
